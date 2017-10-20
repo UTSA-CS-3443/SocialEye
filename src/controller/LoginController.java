9e0d5e4237cc703277ebb4c70f7b591336f7ad20
666brian <bxb760@my.utsa.edu>
2017-10-19 21:17:16 -0500

    Added logincontroller.java and login.fxml
    logincontroller.java : will be used for our login menu
    
    login.fxml : this is how our login menu will look
    
    As of right now, the login menu's background color, if you guys want it
    to be a different color then let me know

---
+++ src/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class LoginController {
+
+	@FXML TextField user;
+	@FXML PasswordField password;
+	@FXML Button login;
+	
+	
+	
+}
