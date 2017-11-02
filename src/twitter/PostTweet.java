42f1c395ac35608989da82d8d128f532f97a57a1
666brian <31326217+666brian@users.noreply.github.com>
2017-11-02 15:47:37 -0500

    Add files via upload
    allows the user to post tweets

---
+++ src/twitter/PostTweet.java
@@ -0,0 +1,34 @@
+package twitter;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import twitter4j.*;
+
+/**
+ * PostTweet does what it says.
+ * Posts a tweet to the user's twitter. 
+ * 
+ * @author Brian
+ *
+ */
+public class PostTweet 
+{
+    public static void main(String[] args) throws IOException, TwitterException 
+    {
+    	// Used to get input from the user
+        Scanner scan = new Scanner(System.in);
+       // The factory instance is re-useable and thread safe.
+	    Twitter twitter = TwitterFactory.getSingleton();
+	    // Gets input from the user of what they want to tweet
+	    String tweet = scan.next(); 
+	    // Attempt to post a tweet to twitter
+	    try {
+	    	// Post a tweet to your timeline
+	    	Status status = twitter.updateStatus(tweet);
+	    // If we were unable to post a tweet
+	    } catch (TwitterException e) {
+	        System.err.println("Error occurred while trying to post tweet.");
+	    }
+    }
+}
