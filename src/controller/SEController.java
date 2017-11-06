74144a6034b577613b65ad6ee674ed8f95a841fe
666brian <31326217+666brian@users.noreply.github.com>
2017-11-06 12:25:32 -0600

    Add files via upload
    this is the controller for our program. As of right now, it's functioning with the home tab. The other tabs will be implemented soon

---
+++ src/controller/SEController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import twitter.*;
+
+/**
+ * 
+ * 
+ * 
+ * @author Brian
+ *
+ */
+public class SEController 
+{
+	@FXML
+	TabPane SETabPane;
+	@FXML
+	Tab homeTab, twitterTab, facebookTab, redditTab;
+	Stage stage;
+	/**
+	 * 
+	 * 
+	 * 
+	 * @author Brian Le
+	 */
+	public void update() 
+	{
+		
+		// Tab is set to home by default
+		homeTab.setOnSelectionChanged(new EventHandler<Event>() {
+            @Override
+            public void handle(Event t) 
+            {
+                if (homeTab.isSelected()) 
+                {
+                	// Attempt to change the view back to the default view
+        			try {
+        				// change back to the home view
+        				Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+        				Scene scene = new Scene(root,400,400);
+        				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        				Main.stage.setScene(scene);
+        				Main.stage.show();														  
+        			}
+        			// We were unable to set the view back to the default view
+        			catch(Exception e) {
+        				e.printStackTrace(); 
+        			}
+        			
+                }
+            }
+        });
+	}
+}
