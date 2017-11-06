d034b0e0f447bb5d6f5390283e054bbd29520e7a
666brian <31326217+666brian@users.noreply.github.com>
2017-11-06 12:27:13 -0600

    Add files via upload
    Adds a title to our program so when it launches, the name appears at the top. Also, there is code in there to set the application icon when you guys finish making the icon.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	
+	public static Stage stage;
 	public AnchorPane layout;
 	
 	/**
@@ -19,19 +21,25 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//FXMLLoader loader = new FXMLLoader();
-			//loader.setLocation(Main.class.getResource("/SocialEye/src/view/Main.fxml"));
 			Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			// Sets the title of the program
+			// This displays at the top of the application
+			primaryStage.setTitle("SocialEye");
+			// Sets the application icon next to the title of the program
+			//primaryStage.getIcons().add(new Image("/icons/appicon.png"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		this.stage = primaryStage;
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 }
