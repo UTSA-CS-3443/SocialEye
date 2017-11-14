5be85534bd8cd7eb3f1d76ef503a065b5e2436f0
Devin Nguyen <diagonr@gmail.com>
2017-11-14 00:46:33 -0600

    Test functions for reddit code retrieval (for authentication)

---
+++ tests/mainTests/getUserAuthUrlTest.java
@@ -0,0 +1,54 @@
+package mainTests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import controller.RedditOAuth;
+
+public class getUserAuthUrlTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws IOException {
+		if(Desktop.isDesktopSupported())
+		{
+			URL url = new URL(RedditOAuth.getUserAuthUrl("test"));
+			String nullFragment = null;
+			URI uri;
+			try {
+				uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), nullFragment );
+		    	Desktop.getDesktop().browse(uri);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ tests/mainTests/getUserAuthUrlTest02.java
@@ -0,0 +1,54 @@
+package mainTests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import controller.RedditOAuth;
+
+public class getUserAuthUrlTest02 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		try {
+			URL url;
+			url = new URL(RedditOAuth.getUserAuthUrl("test"));
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			int code = connection.getResponseCode();
+			System.out.println(code);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
